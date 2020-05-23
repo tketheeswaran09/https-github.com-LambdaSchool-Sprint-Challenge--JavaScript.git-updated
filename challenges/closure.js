a95806afbfa81626497226ce1d497f059927aaf9
Tharshan Ketheeswaran <tketheeswaran09@gmail.com>
2020-05-23 13:44:48 +0100

    Tharshan-New

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,24 @@ myFunction();
 
 // Explanation: 
 
+/*There is a box. Inside that box there are 2 smaller boxes. The myfunction box belongs to the main box which is the 
+'const extrernal' box only, but the 'nested function' box belongs to both 'myfunction box' and the 'const external'. 
+The problem is the nested function box does not know what internal means. 
+So it decides to look outside the box and figures out it belongs to the external box.*/
+
 
 /* Task 2: Counter */
 
-/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `sumation` that accepts a parameter and uses a counter to return the s
+ummation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function sumation(parameter) {
+  let sum = 0;
+  for (let i = 1; i <= parameter; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumation(4));
+
